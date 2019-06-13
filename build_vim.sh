b922413374a7721bac122f51723402e3c05d427f
Anthony Barbier <anthony.barbier@arm.com>
2019-06-13 10:57:30 +0100

    Added incsearch / vim-dispatch

---
+++ build_vim.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+set -e
+
+sudo apt-get install build-essential cmake python-dev
+./configure \
+   --enable-multibyte \
+   --enable-perlinterp=yes \
+   --enable-pythoninterp=yes \
+   --with-python-config-dir=/usr/lib/python2.7/config-x86_64-linux-gnu \
+   --enable-python3interp=yes \
+   --with-python3-config-dir=/usr/lib/python3.6/config-3.6m-x86_64-linux-gnu \
+   --enable-luainterp \
+   --with-luajit \
+   --enable-cscope \
+   --enable-gui=auto \
+   --with-features=huge \
+   --with-x \
+   --enable-fontset \
+   --enable-largefile \
+   --with-compiledby="Anthony" \
+   --enable-fail-if-missing
+
+make VIMRUNTIMEDIR=/usr/local/share/vim/vim81 -j24
+sudo make install
