1b0881b59e707dc87d1100c3eb139f606c43905c
Anthony Barbier <anthonyb@graphcore.ai>
2019-07-09 13:49:50 +0100

    Build vim directly from the submodule

--- build_vim.sh
+++ build_vim.sh
@@ -1,5 +1,27 @@
 #!/bin/bash
 set -e
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+cd vim-src
+sudo apt-get install build-essential cmake python-dev libperl-dev
+./configure \
+   --enable-multibyte \
+   --enable-perlinterp=yes \
+   --enable-pythoninterp=yes \
+   --with-python-config-dir=/usr/lib/python2.7/config-x86_64-linux-gnu \
+   --enable-python3interp=yes \
+   --with-python3-config-dir=/usr/lib/python3.6/config-3.6m-x86_64-linux-gnu \
+   --enable-cscope \
+   --enable-gui=auto \
+   --with-features=huge \
+   --with-x \
+   --enable-fontset \
+   --enable-largefile \
+   --with-compiledby="Anthony" \
+   --prefix=${DIR}/install \
+   --enable-fail-if-missing
+make -j24
+make install
+exit 0
 
 sudo apt-get install build-essential cmake python-dev
 ./configure \
