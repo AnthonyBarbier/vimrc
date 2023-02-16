56743abb9225b282fa68a4e8ee7018b507a78277
Anthony Barbier <anthonyb@graphcore.ai>
2023-02-16 09:59:33 +0000

    Install vim from conda

---
+++ install_conda_vim.sh
@@ -0,0 +1,19 @@
+curl -L -O "https://github.com/conda-forge/miniforge/releases/latest/download/Mambaforge-$(uname)-$(uname -m).sh"
+bash Mambaforge-$(uname)-$(uname -m).sh -b -p `pwd`/conda
+. `pwd`/conda/etc/profile.d/conda.sh
+
+export PYTHONNOUSERSITE=1
+python_version=$(python3 -c "import platform;print(platform.python_version())")
+buildenv=`pwd`/buildenv
+conda create python=${python_version} vim --prefix $buildenv -y
+
+mkdir -p install/bin
+for v in $buildenv/bin/vim*
+do
+  bin=install/bin/`basename $v`
+  echo ". `pwd`/conda/etc/profile.d/conda.sh" > $bin
+  echo "conda activate $buildenv" >> $bin
+  echo "$v $@" >> $bin
+  chmod +x $bin
+done
+
