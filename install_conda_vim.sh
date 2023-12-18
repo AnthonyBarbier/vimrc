7212411065185d9de52eb720da24e529b64d3493
Anthony Barbier <anthonyb@graphcore.ai>
2023-12-18 10:17:22 +0000

    Install gh in the vim Conda env

--- install_conda_vim.sh
+++ install_conda_vim.sh
@@ -5,10 +5,10 @@ bash Mambaforge-$(uname)-$(uname -m).sh -b -p `pwd`/conda
 export PYTHONNOUSERSITE=1
 python_version=$(python3 -c "import platform;print(platform.python_version())")
 buildenv=`pwd`/buildenv
-conda create python=${python_version} vim --prefix $buildenv -y
+conda create python=${python_version} vim gh --prefix $buildenv -y
 
 mkdir -p install/bin
-for v in $buildenv/bin/vim*
+for v in $(ls $buildenv/bin/vim* $buildenv/bin/gh)
 do
   bin=install/bin/`basename $v`
   echo ". `pwd`/conda/etc/profile.d/conda.sh" > $bin
