827b581ac4de3e6e921699d1c37945737904565b
Anthony Barbier <anthonyb@graphcore.ai>
2024-05-14 15:20:06 +0000

    Install logcli

--- install_conda_vim.sh
+++ install_conda_vim.sh
@@ -3,16 +3,19 @@ curl -o micromamba -L -O "https://github.com/mamba-org/micromamba-releases/relea
 chmod +x micromamba
 
 export PYTHONNOUSERSITE=1
+export MAMBA_ROOT_PREFIX=`pwd`
 python_version=$(python3 -c "import platform;print(platform.python_version())")
 buildenv=`pwd`/buildenv
 micromamba=`pwd`/micromamba
-$micromamba create -c conda-forge python=${python_version} vim gh tree tig --prefix $buildenv -y
+$micromamba create -c conda-forge python=${python_version} vim gh tree tig loki-logcli --prefix $buildenv -y
 mkdir -p install/bin
-for v in $(ls $buildenv/bin/vim* $buildenv/bin/gh $buildenv/bin/tree $buildenv/bin/tig)
+for v in $(ls $buildenv/bin/vim* $buildenv/bin/gh $buildenv/bin/tree $buildenv/bin/tig $buildenv/bin/logcli)
 do
   bin=install/bin/`basename $v`
   echo "" > $bin
   echo "$micromamba run -p $buildenv $v \"\$@\"" >> $bin
   chmod +x $bin
 done
-
+# Could make them all the same and only base it on the name
+# cmd=$(basename $0)
+# /nethome/anthonyb/.vim/micromamba run -p /nethome/anthonyb/.vim/buildenv /nethome/anthonyb/.vim/buildenv/bin/${cmd} "$@"
